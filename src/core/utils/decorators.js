f4fd854624271d1bfd4b9e8602c52d4a0c444430
simonyan51 <gnelsimonyan51@gmail.com>
2018-12-17 18:04:36 +0400

    Changed Inject decorator

--- src/core/utils/decorators.js
+++ src/core/utils/decorators.js
@@ -1,11 +1,9 @@
 import Injector from '../injector';
 
 const Injectable = (isTransient) => target => Injector.registerInjection(target.name, target, isTransient);
-const Inject = (...args) => target => {
-    return (...constructorParams) => {
-        const constructor = Injector.resolve(args, target);
-        return new constructor(...constructorParams);
-    }
+const Inject = (...args) => target => (...constructorParams) => {
+    const constructor = Injector.resolve(args, target);
+    return new constructor(...constructorParams);
 };
 
 export {
